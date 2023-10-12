89b7ebd662e22aa4a8a0c2631861f8918e496e49
Patrick Coxall <mr.coxall@mths.ca>
2023-10-12 15:05:08 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,44 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program compares random numbers.
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber1: number = 0
+let randomNumber2: number = 0
+
+randomNumber1 = randint(1, 99)
+randomNumber2 = randint(1, 99)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// show 1st number
+input.onButtonPressed(Button.A, function() {
+  basic.clearScreen()
+  basic.showString("#1: " + randomNumber1.toString())
+  basic.showIcon(IconNames.Happy)
+})
+
+// show 2nd number
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  basic.showString("#2: " + randomNumber2.toString())
+  basic.showIcon(IconNames.Happy)
+})
+
+// show comparison
+input.onGesture(Gesture.Shake, function () {
+  basic.clearScreen()
+
+  // randomNumber compare
+  if (randomNumber1 < randomNumber2) {
+    basic.showString(randomNumber1.toString() + " < " + randomNumber2.toString())
+  } else {
+      basic.showString(randomNumber1.toString() + " > " + randomNumber2.toString())
+  }
+
+  // pasue and show you are ready again
+  basic.pause(1000)
+  basic.showIcon(IconNames.Sad)
+})
